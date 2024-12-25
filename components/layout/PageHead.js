80e082c89ff23d8cc206a4979c6356d6f0756cbd
harshjf <harsh@juniperforge.com>
2024-12-25 16:58:28 +0530

    Sustech - Tab Title change

--- components/layout/PageHead.js
+++ components/layout/PageHead.js
@@ -4,9 +4,7 @@ const PageHead = ({ headTitle }) => {
   return (
     <>
       <Head>
-        <title>
-          {headTitle ? headTitle : "Sustech Neo Energy Private Ltd."}
-        </title>
+        <title>{headTitle ? headTitle : "Sustech Neo Energy"}</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700;800&family=Poppins:wght@400;500;600;700;800&display=swap"
           rel="stylesheet"
